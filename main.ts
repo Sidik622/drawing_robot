89862edb625bf28cd285a3e1f8d2ea546e13cd30
Sidik622 <110263995+Sidik622@users.noreply.github.com>
2023-08-26 01:27:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    sparkbitO.stopMotorModule(SparkbitOutPort.Output2)
+    sparkbitO.setLightModule(SparkbitOutPort.Output3, SparkbitColor.Red, 0)
+    music.stopAllSounds()
+})
 basic.forever(function () {
     if (sparkbitI.bumpSensorIsPressed(SparkbitInPort.Input1)) {
         sparkbitO.rotateMotorModule(SparkbitOutPort.Output2, SparkbitDirection.Clockwise, 36)
         sparkbitO.setLightModule(SparkbitOutPort.Output3, SparkbitColor.Green, 49)
+        music.setVolume(27)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 150), music.PlaybackMode.LoopingInBackground)
     }
 })
